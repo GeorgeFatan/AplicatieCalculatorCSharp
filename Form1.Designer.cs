4417980c9b8c7c3476ae19ca1d50ab318eca4fe6
GeorgeFatan <127293166+GeorgeFatan@users.noreply.github.com>
2025-08-04 14:53:33 +0300

    Design-ul a fost terminat.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox = new TextBox();
+            buttonSeven = new Button();
+            buttonEight = new Button();
+            buttonNine = new Button();
+            buttonFour = new Button();
+            buttonFive = new Button();
+            buttonSix = new Button();
+            buttonOne = new Button();
+            buttonTwo = new Button();
+            buttonThree = new Button();
+            buttonDivide = new Button();
+            this.buttonMultiply = new Button();
+            this.buttonPlus = new Button();
+            this.buttonMinus = new Button();
+            this.buttonEqual = new Button();
+            this.buttonClear = new Button();
+            buttonZero = new Button();
+            SuspendLayout();
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(193, 93);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(340, 23);
+            textBox.TabIndex = 0;
+            // 
+            // buttonSeven
+            // 
+            buttonSeven.Location = new Point(193, 138);
+            buttonSeven.Name = "buttonSeven";
+            buttonSeven.Size = new Size(75, 23);
+            buttonSeven.TabIndex = 1;
+            buttonSeven.Text = "7";
+            buttonSeven.UseVisualStyleBackColor = true;
+            // 
+            // buttonEight
+            // 
+            buttonEight.Location = new Point(283, 138);
+            buttonEight.Name = "buttonEight";
+            buttonEight.Size = new Size(75, 23);
+            buttonEight.TabIndex = 2;
+            buttonEight.Text = "8";
+            buttonEight.UseVisualStyleBackColor = true;
+            // 
+            // buttonNine
+            // 
+            buttonNine.Location = new Point(377, 138);
+            buttonNine.Name = "buttonNine";
+            buttonNine.Size = new Size(75, 23);
+            buttonNine.TabIndex = 3;
+            buttonNine.Text = "9";
+            buttonNine.UseVisualStyleBackColor = true;
+            // 
+            // buttonFour
+            // 
+            buttonFour.Location = new Point(193, 167);
+            buttonFour.Name = "buttonFour";
+            buttonFour.Size = new Size(75, 23);
+            buttonFour.TabIndex = 4;
+            buttonFour.Text = "4";
+            buttonFour.UseVisualStyleBackColor = true;
+            // 
+            // buttonFive
+            // 
+            buttonFive.Location = new Point(283, 167);
+            buttonFive.Name = "buttonFive";
+            buttonFive.Size = new Size(75, 23);
+            buttonFive.TabIndex = 5;
+            buttonFive.Text = "5";
+            buttonFive.UseVisualStyleBackColor = true;
+            // 
+            // buttonSix
+            // 
+            buttonSix.Location = new Point(377, 167);
+            buttonSix.Name = "buttonSix";
+            buttonSix.Size = new Size(75, 23);
+            buttonSix.TabIndex = 6;
+            buttonSix.Text = "6";
+            buttonSix.UseVisualStyleBackColor = true;
+            // 
+            // buttonOne
+            // 
+            buttonOne.Location = new Point(193, 196);
+            buttonOne.Name = "buttonOne";
+            buttonOne.Size = new Size(75, 23);
+            buttonOne.TabIndex = 7;
+            buttonOne.Text = "1";
+            buttonOne.UseVisualStyleBackColor = true;
+            // 
+            // buttonTwo
+            // 
+            buttonTwo.Location = new Point(283, 196);
+            buttonTwo.Name = "buttonTwo";
+            buttonTwo.Size = new Size(75, 23);
+            buttonTwo.TabIndex = 8;
+            buttonTwo.Text = "2";
+            buttonTwo.UseVisualStyleBackColor = true;
+            // 
+            // buttonThree
+            // 
+            buttonThree.Location = new Point(377, 196);
+            buttonThree.Name = "buttonThree";
+            buttonThree.Size = new Size(75, 23);
+            buttonThree.TabIndex = 9;
+            buttonThree.Text = "3";
+            buttonThree.UseVisualStyleBackColor = true;
+            // 
+            // buttonDivide
+            // 
+            buttonDivide.Location = new Point(458, 138);
+            buttonDivide.Name = "buttonDivide";
+            buttonDivide.Size = new Size(75, 23);
+            buttonDivide.TabIndex = 10;
+            buttonDivide.Text = "/";
+            buttonDivide.UseVisualStyleBackColor = true;
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new Point(458, 167);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new Size(75, 23);
+            this.buttonMultiply.TabIndex = 11;
+            this.buttonMultiply.Text = "*";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new Point(458, 225);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new Size(75, 23);
+            this.buttonPlus.TabIndex = 12;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new Point(458, 196);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new Size(75, 23);
+            this.buttonMinus.TabIndex = 14;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            // 
+            // buttonEqual
+            // 
+            this.buttonEqual.Location = new Point(377, 225);
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.Size = new Size(75, 23);
+            this.buttonEqual.TabIndex = 15;
+            this.buttonEqual.Text = "=";
+            this.buttonEqual.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new Point(193, 225);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new Size(75, 23);
+            this.buttonClear.TabIndex = 16;
+            this.buttonClear.Text = "C";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // buttonZero
+            // 
+            buttonZero.Location = new Point(283, 225);
+            buttonZero.Name = "buttonZero";
+            buttonZero.Size = new Size(75, 23);
+            buttonZero.TabIndex = 17;
+            buttonZero.Text = "0";
+            buttonZero.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonZero);
+            Controls.Add(this.buttonClear);
+            Controls.Add(this.buttonEqual);
+            Controls.Add(this.buttonMinus);
+            Controls.Add(this.buttonPlus);
+            Controls.Add(this.buttonMultiply);
+            Controls.Add(buttonDivide);
+            Controls.Add(buttonThree);
+            Controls.Add(buttonTwo);
+            Controls.Add(buttonOne);
+            Controls.Add(buttonSix);
+            Controls.Add(buttonFive);
+            Controls.Add(buttonFour);
+            Controls.Add(buttonNine);
+            Controls.Add(buttonEight);
+            Controls.Add(buttonSeven);
+            Controls.Add(textBox);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox;
+        private Button buttonSeven;
+        private Button buttonEight;
+        private Button buttonNine;
+        private Button buttonFour;
+        private Button buttonFive;
+        private Button buttonSix;
+        private Button buttonOne;
+        private Button buttonTwo;
+        private Button buttonThree;
+        private Button buttonDivide;
+        private Button buttonZero;
+        private Button buttonClear;
+        private Button buttonEqual;
+        private Button buttonMinus;
+        private Button buttonPlus;
+        private Button buttonMultiply;
     }
 }
